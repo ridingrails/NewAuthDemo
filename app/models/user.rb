240ba81ae962e6896d7afabe2f7d29f0be90e077
App Academy Student <student@appacademy.io>
2013-12-12 09:40:46 -0800

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   def self.find_by_credentials(username, password)
     user = User.find_by_username(username)
 
-#    return nil if user.nil?
+    return nil if user.nil?
 
     user.is_password?(password) ? user : nil
   end
